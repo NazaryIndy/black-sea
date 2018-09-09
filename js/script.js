24f01e84146f68ee50263493f00e569fc9d39d22
NazaryIndy <35665349+NazaryIndy@users.noreply.github.com>
2018-09-09 18:28:50 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+// верхнее меню
+
 var navMain = document.querySelector('.main-menu');
-  var navToggle = document.querySelector('.menu-button');
+var navToggle = document.querySelector('.menu-button');
 
   navToggle.addEventListener('click', function() {
     if (navMain.classList.contains('main-menu-closed')) {
@@ -10,3 +12,28 @@ var navMain = document.querySelector('.main-menu');
       navMain.classList.remove('main-menu-opened');
     }
   });
+
+// карточка товара
+
+var open = document.querySelector('.item-det');
+var card = document.querySelector('.modal-card');
+var overlay = document.querySelector('.modal-overlay');
+var close = document.querySelector('.modal-close');
+
+
+open.addEventListener('click', function(def) {
+  def.preventDefault();
+  card.classList.add('modal-show');
+  overlay.classList.remove('visually-hidden');
+});
+
+
+close.addEventListener('click', function(def) {
+  def.preventDefault();
+  card.classList.remove('modal-show');
+  overlay.classList.add('visually-hidden');
+});
+
+
+
+
